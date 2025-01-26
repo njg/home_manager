1669ca7a33fe422cbce90d3627c79d5d587339ce
Nathan Goldschmidt <nathan.goldschmidt@gmail.com>
2025-01-25 23:51:11 -0500

    works for now, remember to check pinentry program

--- home.nix
+++ home.nix
@@ -68,7 +68,7 @@ in
     # clang
     gnumake
     cmake
-    ((emacsPackagesFor emacs30).emacsWithPackages (epkgs: with epkgs; [ vterm
+    ((emacsPackagesFor emacs29).emacsWithPackages (epkgs: with epkgs; [ vterm
                                                                         saveplace-pdf-view
                                                                         pdf-tools
                                                                         nov
@@ -131,32 +131,33 @@ in
 
 
     #  https://github.com/NixOS/nixpkgs/issues/186570
-    (let cura5 = appimageTools.wrapType2 rec {
-           name = "cura5";
-           version = "5.7.0";
-           src = fetchurl {
-             url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-X64.AppImage";
-             hash = "sha256-5PaBhPJKqa8LxEHTRNTLqkcIfC2PkqmTWx9c1+dc7k0=";
-           };
-           extraPkgs = pkgs: with pkgs; [ ];
-         }; in writeScriptBin "cura" ''
-      #! ${pkgs.bash}/bin/bash
-      # AppImage version of Cura loses current working directory and treats all paths relateive to $HOME.
-      # So we convert each of the files passed as argument to an absolute path.
-      # This fixes use cases like `cd /path/to/my/files; cura mymodel.stl anothermodel.stl`.
-      args=()
-      for a in "$@"; do
-        if [ -e "$a" ]; then
-          a="$(realpath "$a")"
-        fi
-        args+=("$a")
-      done
-      exec "${cura5}/bin/cura5" "''${args[@]}"
-    '')
+    # (let cura5 = appimageTools.wrapType2 rec {
+    #        name = "cura5";
+    #        version = "5.7.0";
+    #        src = fetchurl {
+    #          url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-X64.AppImage";
+    #          hash = "sha256-5PaBhPJKqa8LxEHTRNTLqkcIfC2PkqmTWx9c1+dc7k0=";
+    #        };
+    #        extraPkgs = pkgs: with pkgs; [ ];
+    #      }; in writeScriptBin "cura" ''
+    #   #! ${pkgs.bash}/bin/bash
+    #   # AppImage version of Cura loses current working directory and treats all paths relateive to $HOME.
+    #   # So we convert each of the files passed as argument to an absolute path.
+    #   # This fixes use cases like `cd /path/to/my/files; cura mymodel.stl anothermodel.stl`.
+    #   args=()
+    #   for a in "$@"; do
+    #     if [ -e "$a" ]; then
+    #       a="$(realpath "$a")"
+    #     fi
+    #     args+=("$a")
+    #   done
+    #   exec "${cura5}/bin/cura5" "''${args[@]}"
+    # '')
 
   ];
 
   home.activation.emacs-org-link = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  $DRY_RUN_CMD mkdir -p $VERBOSE_ARG $HOME/gdrive
   $DRY_RUN_CMD ln -Tsf $VERBOSE_ARG $HOME/gdrive/org $HOME/org
   '';
 
@@ -253,6 +254,6 @@ in
     enable = true;
     defaultCacheTtl = 1800;
     enableSshSupport = true;
-    # pinentryPackage = "emacs";
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 }
